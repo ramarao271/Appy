a48e922d7479d2ca5e614017297a6a883649e6c8
ramarao271 <ramarao.study@gmail.com>
2015-08-14 18:20:59 +0000

    first

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.api_key = "ccec25f6b73bd6dfa81f4931a8b9b381"
+  config.secret = "3ebf4330b824c33ed1a756d1c7f11e92"
+  config.redirect_uri = "https://shopify-ramarao271.c9.io/auth/shopify/callback"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
