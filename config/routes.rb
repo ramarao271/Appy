2d8e45c3386495dd9267a0739045676ecddfb479
ramarao271 <ramarao.study@gmail.com>
2015-10-22 06:45:24 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ Rails.application.routes.draw do
     get '/getPresets/:customer_id' => 'custom_tailorings#getPresets', :as => "get_presets"
   end
 
-  root :to => 'home#index'
+  root :to => 'products#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
