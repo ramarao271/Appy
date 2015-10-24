617c835eff3c06c8ec7fb6916bf89107b54c2eef
ramarao271 <ramarao.study@gmail.com>
2015-10-24 15:52:53 +0000

    customerAccount

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,9 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+  respond_to do |format|
+    format.json {render :json => @products.to_json(:include => :variants)  }    
+    end    
   end
 
   # GET /products/1
