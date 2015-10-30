4496c00d8a6e88bf47f50914f5755c93ea62e711
ramarao271 <ramarao.study@gmail.com>
2015-10-30 10:24:01 +0000

    referral

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-:address => 'gmail.com',
+:address => 'smtp.gmail.com',
+:domain => 'gmail.com',
 :port => 587,
 :user_name => "ramarao.study@gmail.com",
 :password => "sapts.com",
