e63a93719afaf6d78a5100ca4b0665f9250ffcd2
ramarao271 <ramarao.study@gmail.com>
2015-11-01 06:02:17 +0000

    referral

--- db/migrate/20151101055818_change_joined_of_customer_refer_email.rb
+++ db/migrate/20151101055818_change_joined_of_customer_refer_email.rb
@@ -1,6 +1,6 @@
 class ChangeJoinedOfCustomerReferEmail < ActiveRecord::Migration
   def change
-    change_column :customer_refer_email, :joined, :string
-    rename_column :customer_refer_email, :joined, :status
+    change_column :customer_refer_emails, :joined, :string
+    rename_column :customer_refer_emails, :joined, :status
   end
 end
