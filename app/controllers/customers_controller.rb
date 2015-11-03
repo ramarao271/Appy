ba8a25651df6b2c1a3f4e16905271eafdafa05f8
ramarao271 <ramarao.study@gmail.com>
2015-11-03 14:54:45 +0530

    windows

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,7 +10,7 @@ class CustomersController < ApplicationController
     @customer.to_yaml         
     @response="OLD_USER"
     if @customer.nil?
-      @customer=ShopifyAPI::Customer.where("email=?",params[:email])
+      @customer=ShopifyAPI::Customer.search(query: "email:#{params[:email]}")
       @customer.to_yaml
       if @customer.nil?
         @response="NEW_USER"
