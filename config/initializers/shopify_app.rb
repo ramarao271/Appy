5a82c5b66b59352a7637042dffaf412b081b7080
ramarao271 <ramarao.study@gmail.com>
2015-11-14 10:00:02 +0000

    apps

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = "e7addfa43611d064b289ace6c592636e"
   config.secret = "1727fabbede4785bc6e07a4013ef4df6"
   config.redirect_uri = "http://vavarna.herokuapp.com/auth/shopify/callback/"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products,read_customers"
   config.embedded_app = true
-  
 end
