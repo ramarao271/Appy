e33c110e0dd88ce29237788decfd3999bc1e6251
ramarao271 <ramarao.study@gmail.com>
2015-11-14 07:53:14 +0000

    apps

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,13 @@
 class Shop < ActiveRecord::Base
   include ShopifyApp::Shop
+   def self.store(session)
+    shop = self.new(domain: session.url, token: session.token)
+    shop.save!
+    shop.id
+   end
+   def self.retrieve(id)
+    return if id.blank?
+    shop = Shop.find(id)
+    ShopifyAPI::Session.new(shop.domain, shop.token)
+   end
 end
--- config/initializers/shopify_session_repository.rb
+++ config/initializers/shopify_session_repository.rb
@@ -6,18 +6,18 @@
 # Interface to implement are self.retrieve(id) and self.store(ShopifyAPI::Session)
 # Here is how you would add these functions to an ActiveRecord:
 #
-class Shop < ActiveRecord::Base
-  def self.store(session)
-    shop = self.new(domain: session.url, token: session.token)
-    shop.save!
-    shop.id
-  end
+# class Shop < ActiveRecord::Base
+#   def self.store(session)
+#     shop = self.new(domain: session.url, token: session.token)
+#     shop.save!
+#     shop.id
+#   end
 
-  def self.retrieve(id)
-    if shop = self.where(id: id).first
-      ShopifyAPI::Session.new(shop.domain, shop.token)
-    end
-  end
-end
+#   def self.retrieve(id)
+#     if shop = self.where(id: id).first
+#       ShopifyAPI::Session.new(shop.domain, shop.token)
+#     end
+#   end
+# end
 
 ShopifyApp::SessionRepository.storage = Shop
