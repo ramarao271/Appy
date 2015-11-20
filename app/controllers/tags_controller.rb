032c14a0a405bf057a2400531c113f368489e6f8
ramarao271 <ramarao.study@gmail.com>
2015-11-20 20:19:28 +0000

    tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -13,7 +13,7 @@ class TagsController < ApplicationController
 
   def create_tags
     tags_array=["Banaras Silk","Chanderi Silk","Kanchipuram Silk","Uppada Silk","Gadwal Silk","Coimbatore Silk","Venkatagiri Silk","Mysore Silk","Assam Silk","Mangalagiri Silk","Ikat Silk","Ksheerapuri Silk","Kota Silk","Crepe Silk","Faux Crepe","Chiffon","Georgette","Faux Georgette","Banaras Fancy","Kora Fancy","Art Silk","Super Net","Kota Fancy","Chanderi Fancy","Organza Silk","Organza Cotton","Kora Cotton","Bengal Cotton","Dhaka Cotton","Chanderi Cotton","Banaras Cotton","Coimbatore Cotton","Gadwal Cotton","Kanchi Cotton","Uppada Cotton","Mangalagiri Cotton"]
-    Tag.delete_all
+    #Tag.delete_all
     tags_array.each { |tag|
       
       tagDb=Tag.new
