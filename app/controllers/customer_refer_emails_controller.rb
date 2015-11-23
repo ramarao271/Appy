072b4b9342b0b1be88f0168223dd08c333a60400
ramarao271 <ramarao.study@gmail.com>
2015-11-23 17:10:07 +0000

    tags

--- app/controllers/customer_refer_emails_controller.rb
+++ app/controllers/customer_refer_emails_controller.rb
@@ -38,6 +38,7 @@ class CustomerReferEmailsController < ApplicationController
         @customer_refer_email.customer_id=params[:customer_id]
         @customer_refer_email.status="REFERRED"
         @customer_refer_email.no_of_times_sent=1
+        @customer_refer_email.medium="Gmail"
         @customer.customer_refer_emails << @customer_refer_email
         UserMailer.send_refer_email(@customer,@customer_refer_email).deliver_now
       end  
