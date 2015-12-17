8f4d5630fd0ab8390cd93ed542f9968490316776
ramarao271 <ramarao.study@gmail.com>
2015-12-17 02:41:24 +0000

    test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,7 +4,7 @@ class TestsController < ApplicationController
         shop_session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
         #puts shop_session.to_yaml
         ShopifyAPI::Base.activate_session(shop_session)
-        customer=Customer.where("email=?","ramarao.project@gmail.com")
+        customer=ShopifyAPI::Customer.find(2212122116)
         customer.save
     end
     def test
