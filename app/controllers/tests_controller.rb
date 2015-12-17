e8ff41b3c83bf585c880799a52f32d313c0a849d
ramarao271 <ramarao.study@gmail.com>
2015-12-17 03:04:56 +0000

    test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,8 +4,17 @@ class TestsController < ApplicationController
         shop_session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
         #puts shop_session.to_yaml
         ShopifyAPI::Base.activate_session(shop_session)
-        customer=ShopifyAPI::Customer.find(2212122116)
-        customer.save
+        count=ShopifyAPI::Customer.count
+        calls=count/50
+        calls=calls+1
+        i=1
+        calls.times do 
+            customers = ShopifyAPI::Customer.find(:all,:params => {:limit => 250,:page => i})
+            i=i+1
+            customers.each do |customer|
+                customer.save
+            end
+        end    
     end
     def test
     end
