d8062926e05d6b5212ecc697ddbc1871e13f19a9
ramarao271 <ramarao.study@gmail.com>
2016-01-02 08:57:31 +0000

    tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,7 +4,7 @@ class TagsController < ApplicationController
   # GET /tags
   # GET /tags.json
   def index
-    @tags = Tag.where(@shop_session.url)
+    @tags = Tag.where("shop=?",@shop_session.url)
   end
   # GET /tags/1
   # GET /tags/1.json
