b859f9e4135378928be047704d84ffaa1e90319d
ramarao271 <ramarao.study@gmail.com>
2016-01-17 04:28:03 +0000

    ct

--- db/migrate/20160117032229_change_product_id_of_variants.rb
+++ db/migrate/20160117032229_change_product_id_of_variants.rb
@@ -1,6 +1,6 @@
 class ChangeProductIdOfVariants < ActiveRecord::Migration
   def change
-    change_column :variants, :product_id, :decimal, :precision => 15, :scale => 0
+    change_column :variants, :product_id, :decimal, USING product_id::numeric(15,0)
     change_column :variants, :product_id, :string
   end
 end
