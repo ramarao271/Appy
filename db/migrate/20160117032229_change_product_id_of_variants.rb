35b158323ab9ca3d6bbdadc7250b562573cb0260
ramarao271 <ramarao.study@gmail.com>
2016-01-23 18:02:49 +0000

    cust

--- db/migrate/20160117032229_change_product_id_of_variants.rb
+++ db/migrate/20160117032229_change_product_id_of_variants.rb
@@ -1,6 +1,6 @@
 class ChangeProductIdOfVariants < ActiveRecord::Migration
   def change
-    change_column :variants, :product_id, :decimal, USING product_id::numeric(15,0)
+    change_column :variants, :product_id, 'decimal USING CAST(product_id AS numeric(15,0))'
     change_column :variants, :product_id, :string
   end
 end
