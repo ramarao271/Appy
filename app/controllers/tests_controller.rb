51635a55a404372c7df244c66e0dfb12c38c378f
ramarao271 <ramarao.study@gmail.com>
2016-04-21 11:30:57 +0000

    csv

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,75 +1,88 @@
 class TestsController < ApplicationController
     around_filter :shopify_session
     def get_test
-        count=ShopifyAPI::Product.count
-    calls=count/50
-    calls=calls+1
-    i=1
-    calls.times do 
-      products = ShopifyAPI::Product.find(:all,:params => {:limit => 250,:page => i})
-      i=i+1
-      pcount=0
-      products.each do |product|
-        pcount=pcount+1
-        tags=product.tags
-        price_range="0"
-        price_array=[]
-        productDb=Product.find_by product_id: product.id
-        if productDb.nil?
-          productDb=Product.new
-        end  
-        productDb.product_id=product.id
-        productDb.title=product.title
-        productDb.vendor=product.vendor
-        productDb.save
-        #puts product.variants.to_yaml
-        #arr.map! {|item| item * 3}
-        product.variants.each do |variant|
-          price=variant.price.to_i
-          range_count=0
-          range_value=0
-        while range_count <= 20
-          range_value2=range_value+10
-          if price > range_value && price < range_value2 
-            price_range="price-"+range_value.to_s+"-"+range_value2.to_s
-          end
-          range_count=range_count+1
-          range_value=range_value+10
-        end
+        @products = Product.all
         
-        if price_range!="0"
-          price_array.push(price_range)
-        end
-          variantDb=Variant.find_by variant_id: variant.id
-          if variantDb.nil?
-            variantDb=Variant.new
-          end
-          variantDb.variant_id=variant.id
-          variantDb.product_id=variant.product_id
-          variantDb.title=variant.title
-          variantDb.price=variant.price
-          variantDb.save
-        end
-        puts "tags are"
-        puts tags
-        tags_array=tags.split(",")
-        tags=""
-        tags_array.each do |tg|
-          if !tg.include? "price-"
-            tags=tags+","+tg
-          end  
-        end
-        price_range=price_array.join(",")
-        product.tags=tags+","+price_range
-        puts "tags for product  "
-        puts pcount
-        puts product.tags
-        sleep 1
-        product.save
-        # if pcount >50
-        #   return
-        # end
-      end
+#       @products = User.find(:all)
+#     csv_string = CSV.generate do |csv|
+#          csv << ["product Id", "Name", "Email","Role"]
+#          @products.each do |user|
+#           csv << [user.id, user.name, user.name, user.role]
+#          end
+#     end         
+  
+#   send_data csv_string,
+#   :type => 'text/csv; charset=iso-8859-1; header=present',
+#   :disposition => "attachment; filename=users.csv"   
+    #     count=ShopifyAPI::Product.count
+    # calls=count/50
+    # calls=calls+1
+    # i=1
+    # calls.times do 
+    #   products = ShopifyAPI::Product.find(:all,:params => {:limit => 250,:page => i})
+    #   i=i+1
+    #   pcount=0
+    #   products.each do |product|
+    #     pcount=pcount+1
+    #     tags=product.tags
+    #     price_range="0"
+    #     price_array=[]
+    #     productDb=Product.find_by product_id: product.id
+    #     if productDb.nil?
+    #       productDb=Product.new
+    #     end  
+    #     productDb.product_id=product.id
+    #     productDb.title=product.title
+    #     productDb.vendor=product.vendor
+    #     productDb.save
+    #     #puts product.variants.to_yaml
+    #     #arr.map! {|item| item * 3}
+    #     product.variants.each do |variant|
+    #       price=variant.price.to_i
+    #       range_count=0
+    #       range_value=0
+    #     while range_count <= 20
+    #       range_value2=range_value+10
+    #       if price > range_value && price < range_value2 
+    #         price_range="price-"+range_value.to_s+"-"+range_value2.to_s
+    #       end
+    #       range_count=range_count+1
+    #       range_value=range_value+10
+    #     end
+        
+    #     if price_range!="0"
+    #       price_array.push(price_range)
+    #     end
+    #       variantDb=Variant.find_by variant_id: variant.id
+    #       if variantDb.nil?
+    #         variantDb=Variant.new
+    #       end
+    #       variantDb.variant_id=variant.id
+    #       variantDb.product_id=variant.product_id
+    #       variantDb.title=variant.title
+    #       variantDb.price=variant.price
+    #       variantDb.save
+    #     end
+    #     puts "tags are"
+    #     puts tags
+    #     tags_array=tags.split(",")
+    #     tags=""
+    #     tags_array.each do |tg|
+    #       if !tg.include? "price-"
+    #         tags=tags+","+tg
+    #       end  
+    #     end
+    #     price_range=price_array.join(",")
+    #     product.tags=tags+","+price_range
+    #     puts "tags for product  "
+    #     puts pcount
+    #     puts product.tags
+    #     sleep 1
+    #     product.save
+    #     # if pcount >50
+    #     #   return
+    #     # end
+    #   end
     end
     redirect_to '/products/'
         # shop=Shop.first
